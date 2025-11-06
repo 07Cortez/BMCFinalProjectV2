34d5f0862cc1bba1d7542221c10b3f673bff1a65
07Cortez <168712331+07Cortez@users.noreply.github.com>
2025-11-07 01:19:49 +0800

    Flutter 3.32.5 V Firebase Integration Part 2

--- ecommerce_app1/lib/main.dart
+++ ecommerce_app1/lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:ecommerce_app1/providers/cart_provider.dart';
-import 'package:ecommerce_app1/screens/auth_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:ecommerce_app2/screens/auth_wrapper.dart';
+import 'package:provider/provider.dart';
+import 'package:ecommerce_app2/providers/cart_provider.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -19,7 +20,6 @@ void main() async {
       child: const MyApp(),
     ),
   );
-
   FlutterNativeSplash.remove();
 }
 
@@ -30,40 +30,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Vegan Food Store',
+      title: 'eCommerce App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.green,
-          accentColor: Colors.green[500],
-          brightness: Brightness.light,
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green[700],
-          foregroundColor: Colors.white,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green[700],
-            foregroundColor: Colors.white,
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.green[700],
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.green[700]!),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.green[500]!, width: 2.0),
-          ),
-          labelStyle: TextStyle(color: Colors.green[700]),
-        ),
+        primarySwatch: Colors.deepPurple,
       ),
       home: const AuthWrapper(),
     );
